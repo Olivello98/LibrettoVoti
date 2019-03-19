e9658b7c8142332d180d7db5a6abefea586af504
claud <claud@172.21.56.165>
2019-03-19 12:02:21 +0100

    completato punto 4

--- src/it/polito/tdp/libretto/Libretto.java
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -1,5 +1,6 @@
 package it.polito.tdp.libretto;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,8 +13,55 @@ public class Libretto {
 	}
 		
 	public void add(Voto v) {
+		  for(Voto v1 : voti) {
+			  if((v.getCorso().compareTo(v1.getCorso())==0)&&(v.getPunti()!=v1.getPunti())) {
+				  System.out.println("Corso già presente con voto differente\n");
+				  break;
+			  }
+			  if((v.getCorso().compareTo(v1.getCorso())==0)&&(v.getPunti()==v1.getPunti())){
+			  System.out.println("Corso e relativo voto gia salvati\n");
+			  break;
+		  }
+		}  
+		voti.add(v);
 		
 	}
+	/**
+	 * 
+	 * @param punti punteggio da ricerca
+	 * @return lista di {@link Voto} aventi quel punteggio(eventualmente vuota)
+	 */
+public List<Voto> cercaVoti(int punti){
+	List<Voto> result = new ArrayList<Voto>();
+	for(Voto v : this.voti) {
+		if(v.getPunti()==punti) {
+			result.add(v);
+		}
+	}
+	return result;
+}
 
-	
+public Voto cercaEsame(String nomeEsame) {
+	for(Voto v : this.voti) {
+		if(v.getCorso().compareTo(nomeEsame)==0) {
+			return v;
+		}
+	}
+	return null;
+}
+public Voto votoGiaPresente(Voto voto) {
+	  for(Voto v : voti) {
+		  if((v.getCorso().compareTo(voto.getCorso())==0)&&(v.getPunti()!=voto.getPunti())) {
+			  System.out.println("Corso già presente con voto differente\n");
+			  return null;
+		  }
+		  if((v.getCorso().compareTo(voto.getCorso())==0)&&(v.getPunti()==voto.getPunti())){
+		  System.out.println("Corso e relativo voto gia salvati\n");
+		  return v;
+	  }
+	}  
+	  voti.add(voto);
+	  System.out.println("Nuovo voto aggiunto\n");
+	  return voto;
+}
 }
